13c2d513ab4803711a9bf988bc93546dd3b91478
Armin <armin@wurzweb.com>
2021-04-26 20:23:12 +0200

    improve question

--- app/lib/controllers/show_user.rb
+++ app/lib/controllers/show_user.rb
@@ -7,7 +7,7 @@ module Controllers
     def call(params)
       self.format = :json
 
-      # Question: What is the dry-system-way to get the Models::User Class? 
+      # Question: What is the dry-system-way to inject the Models::User Class i.e. as App["user_repo"]? 
       # Should it be registered manually? The auto_register feature registers instances. 
 
       user = Models::User.with_pk!(params.get(:id))
